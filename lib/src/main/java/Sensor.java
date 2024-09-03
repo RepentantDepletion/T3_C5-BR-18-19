71c03a67eb6def86f2737399ff47b9fa4cd6ca11
Trystan11 <trystan.frederick@students.mq.edu.au>
2024-09-03 10:55:01 +1000

    light sensor
    partially done

--- lib/src/main/java/Sensor.java
+++ lib/src/main/java/Sensor.java
@@ -1,6 +1,17 @@
+interface lightInput { // these names will change based on other classes
+    double getLedColour();
+}
+
 public class Sensor {
 
     double red;
     double green;
     double blue;
+    private lightInput input;
+
+    // constructors
+    public Sensor(lightInput input) {
+        this.input = input;
+    }
+
 }
