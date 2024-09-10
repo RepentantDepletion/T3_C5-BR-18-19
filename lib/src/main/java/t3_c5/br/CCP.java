81a2821e0b91a283af0fb09a37a5ff2b8651040c
Trystan11 <trystan.frederick@students.mq.edu.au>
2024-09-10 21:33:18 +1000

    some stuff added to ccp

--- lib/src/main/java/t3_c5/br/CCP.java
+++ lib/src/main/java/t3_c5/br/CCP.java
@@ -1,7 +1,18 @@
 package t3_c5.br;
 
+import org.checkerframework.checker.units.qual.Acceleration;
+
 public class CCP {
     public static void main(String args[]){
-        
+
+ CarriageControlProgram input;
+
+ CommunicationFromMCP mcpInput;
+ CommunicationToMCP mcpOutput;
+
+ DataProcessing data;
+ Acceleration accInput;
+// Sensor senInput;
+//CCPStatus status;
     }
 }
