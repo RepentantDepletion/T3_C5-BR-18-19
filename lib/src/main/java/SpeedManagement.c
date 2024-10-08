f55b382def837686efb9ca01684f73a5e1dbca58
hotontheblock <aryanaryankumar@outlook.com>
2024-10-08 14:14:10 +1100

    changed my 3 .c files

--- lib/src/main/java/SpeedManagement.c
+++ lib/src/main/java/SpeedManagement.c
@@ -1,29 +1,47 @@
-//package t3_c5.br;
+#include <stdio.h>
+#include <stdlib.h>
 
-public class SpeedTracking {
-    private float speed;
+typedef struct {
+    float currentSpeed;
+    int isSpeedSet;
+} SpeedManagement;
 
-    public SpeedTracking() {
-        this.speed = 0.0f; // Default speed
+void initSpeedManagement(SpeedManagement *sm) {
+    sm->currentSpeed = 0.0;
+    sm->isSpeedSet = 0;
+}
+
+void setSpeed(SpeedManagement *sm, float speed) {
+    // actual speed setting
+    sm->currentSpeed = speed;
+    sm->isSpeedSet = 1; // checks if the speed was set
+    printf("Speed set to: %.2f cm/s\n", sm->currentSpeed);
+}
+
+int confirmSpeedSet(SpeedManagement *sm) {
+    // confirms if the speed was successfully set
+    return sm->isSpeedSet;
+}
+
+float getCurrentSpeed(SpeedManagement *sm) {
+    return sm->currentSpeed;
+}
+
+//function to receive commands from Java
+void receiveCommandFromJava(const char* command) {
+    printf("Received command: %s\n", command);
+}
+
+//test function
+int main() {
+    SpeedManagement sm;
+    initSpeedManagement(&sm);
+
+    setSpeed(&sm, 75.0);
+    if (confirmSpeedSet(&sm)) {
+        printf("Speed was successfully set!\n");
+    } else {
+        printf("Failed to set speed.\n");
     }
-
-    public float getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
-        System.out.println("Speed set to: " + speed);
-    }
-
-    public void increaseSpeed(float increment) {
-        this.speed += increment;
-        System.out.println("Speed increased to: " + speed);
-    }
-
-    public void decreaseSpeed(float decrement) {
-        this.speed -= decrement;
-        if (this.speed < 0) this.speed = 0; // Ensure speed is not negative
-        System.out.println("Speed decreased to: " + speed);
-    }
+    return 0;
 }
