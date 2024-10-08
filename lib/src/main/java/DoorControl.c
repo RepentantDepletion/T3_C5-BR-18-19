f78f5c92fe345b2fe04c52406556768b9633b1fa
Trystan11 <trystan.frederick@students.mq.edu.au>
2024-10-08 14:58:17 +1100

    door control after merge

--- lib/src/main/java/DoorControl.c
+++ lib/src/main/java/DoorControl.c
@@ -21,3 +21,32 @@ public class DoorControl {
         System.out.println("Door closed.");
     }
 }
+#include <ESP32Servo.h>
+#include <stdio.h>
+#include <string.h>
+
+// static const int servoPin = 13; // change servo pin based on what motions setup their 
+
+Servo servo1;
+
+void setup() {
+
+  Serial.begin(115200);
+  servo1.attach(servoPin);
+}
+
+void open() {     // need to add feature that can read whether or not do
+  for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {    // open
+    servo1.write(posDegrees);
+    Serial.println(posDegrees);
+    delay(20);
+  }
+}
+
+void close(){
+for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {    // close
+    servo1.write(posDegrees);
+    Serial.println(posDegrees);
+    delay(20);
+  }
+}
