de9b47724c3593a091771c09d6205b60feb9789c
Trystan11 <trystan.frederick@students.mq.edu.au>
2024-10-15 16:04:44 +1100

    ccp updated

--- lib/src/main/java/CCP.java
+++ lib/src/main/java/CCP.java
@@ -15,13 +15,13 @@ public class CCP {
         BR18 = new CommunicationMCP(socket, input18, "BR18");
         BR19 = new CommunicationMCP(socket2, input19, "BR19");
 
-        input18 = new Parser("OFLN", "10.20.30.118" , 3018, (int) (Math.random() * (10000 - 3000) + 3000));
-        input19 = new Parser("OFLN", "10.20.30.119" , 3019, (int) (Math.random() * (10000 - 3000) + 3000));
+        input18 = new Parser("OFLN", "10.20.30.118", 3018, (int) (Math.random() * (10000 - 3000) + 3000));
+        input19 = new Parser("OFLN", "10.20.30.119", 3019, (int) (Math.random() * (10000 - 3000) + 3000));
 
     }
 
     // DataProcessing data;
-    //Acceleration accInput;
+    // Acceleration accInput;
 
     public static void main(String args[]) {
 
@@ -40,10 +40,29 @@ public class CCP {
             BR18.handshake(); // handshake for br18
             BR19.handshake(); // handshake for br19
 
-
-
         } catch (java.net.SocketException e) {
             System.err.println(e);
         }
     }
+
+    public void heartbeat() {
+
+        while (Time % 2 == 0) {
+            input18.receiveUdpPacket();
+            BR18.sendPacket(input18.toMCP(input18.receiveUdpPacket()));
+
+            input19.receiveUdpPacket();
+            BR19.sendPacket(input19.toMCP(input19.receiveUdpPacket()));
+        }
+    }
+
+    public void call() {
+
+        BR18.recievePacket();
+        input18.fromMCP(BR18.recieved);
+
+        BR19.recievePacket();
+        input19.fromMCP(BR19.recieved);
+    }
+
 }
