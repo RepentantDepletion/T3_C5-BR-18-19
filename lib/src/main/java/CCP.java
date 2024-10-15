ff15e8fa18c23e364a27741008d98b7f6aa16ba8
Trystan11 <trystan.frederick@students.mq.edu.au>
2024-10-15 15:24:07 +1100

    isaacs change

--- lib/src/main/java/CCP.java
+++ lib/src/main/java/CCP.java
@@ -4,21 +4,24 @@ import org.checkerframework.checker.units.qual.Acceleration;
 public class CCP {
 
     static float Time;
+    static Parser input18;
+    static Parser input19;
     static Parser input;
     static CommunicationMCP BR18;
     static CommunicationMCP BR19;
 
     public static void init(DatagramSocket socket, DatagramSocket socket2) {
 
-        input = new Parser();
+        BR18 = new CommunicationMCP(socket, input18, "BR18");
+        BR19 = new CommunicationMCP(socket2, input19, "BR19");
 
-        BR18 = new CommunicationMCP(socket, input, "BR18");
-        BR19 = new CommunicationMCP(socket2, input, "BR19");
+        input18 = new Parser("OFLN", "10.20.30.118" , 3018, (int) (Math.random() * (10000 - 3000) + 3000));
+        input19 = new Parser("OFLN", "10.20.30.119" , 3019, (int) (Math.random() * (10000 - 3000) + 3000));
 
     }
 
     // DataProcessing data;
-    Acceleration accInput;
+    //Acceleration accInput;
 
     public static void main(String args[]) {
 
@@ -36,6 +39,8 @@ public class CCP {
 
             BR18.handshake(); // handshake for br18
             BR19.handshake(); // handshake for br19
+
+
 
         } catch (java.net.SocketException e) {
             System.err.println(e);
