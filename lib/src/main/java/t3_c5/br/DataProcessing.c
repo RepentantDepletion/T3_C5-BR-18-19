ae65ec753234dc938e64a54fa918683e14df7093
ash1k <ashik.hossain1@students.mq.edu.au>
2024-10-22 14:34:52 +1100

    removed comments

--- lib/src/main/java/t3_c5/br/DataProcessing.c
+++ lib/src/main/java/t3_c5/br/DataProcessing.c
@@ -1,175 +1,117 @@
-// DataProcessing.h
-#ifndef DATA_PROCESSING_H
-#define DATA_PROCESSING_H
+#define PACKAGE t3_c5_br
 
-#include "DoorControl.h"
-#include "IRSensor.h"
-#include "LightStatus.h"
-#include "Main.h"
-#include "SpeedManagement.h"
-#include "UltrasonicSensor.h"
-#include "SensorFeedback.h"
+#include "SpeedManagement.c"
+#include "DoorControl.c"
+#include "SensorFeedback.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <jni.h>
 
+
+#define SERVER_PORT_DEFAULT 3018
+#define SERVER_IP "10.20.30.1"
+#define BUFFER_SIZE 1024
+
+// Global Variables
+int sockfd;
+struct sockaddr_in server_addr, local_addr, client_addr;
+socklen_t client_len = sizeof(client_addr);
+
+// DataProcessing struct to hold all components
 typedef struct {
-    DoorControl doorManager;
-    IRSensor irSensor;
-    LightStatus lightStatus;
-    Main main;
     SpeedManagement speedManager;
-    UltrasonicSensor ultrasonicSensor;
-    SensorFeedback sensorFeedback;
+    DoorControl doorManager;
+    SensorFeedback sensorManager;
+    char stationID[MAX_KEY_LENGTH];
 } DataProcessing;
 
-void initDataProcessing(DataProcessing* dp);
-void updateSensorFeedback(DataProcessing* dp);
-
-#endif  // DATA_PROCESSING_H
-
-// DataProcessing.c
-#include "DataProcessing.h"
-
+// Function to initialize the DataProcessing system
 void initDataProcessing(DataProcessing* dp) {
-    initDoorControl(&dp->doorManager);
-    initIRSensor(&dp->irSensor);
-    initLightStatus(&dp->lightStatus);
-    initMain(&dp->main);
+    // Initialize the components
     initSpeedManagement(&dp->speedManager);
-    initUltrasonicSensor(&dp->ultrasonicSensor);
-    initSensorFeedback(&dp->sensorFeedback);
+    initDoorControl(&dp->doorManager);
+    initSensorFeedback(&dp->sensorManager);
+    strcpy(dp->stationID, "Station-01");  // Default station ID
 }
 
-void updateSensorFeedback(DataProcessing* dp) {
-    // Read sensor feedback from multiple sources
-    char doorStatus[10];
-    strcpy(doorStatus, getDoorStatus(&dp->doorManager));
+// Function to start the connection based on Blade Runner ID
+void connectionStartBR(int BR) {
+    int port = (BR == 18) ? 3018 : (BR == 19) ? 3019 : SERVER_PORT_DEFAULT;
 
-    char irSensorStatus[10];
-    strcpy(irSensorStatus, getIRSensorStatus(&dp->irSensor));
+    // Create the socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    char lightStatus[10];
-    strcpy(lightStatus, getLightStatus(&dp->lightStatus));
-
-    char mainStatus[10];
-    strcpy(mainStatus, getMainStatus(&dp->main));
-
-    float currentSpeed = getCurrentSpeed(&dp->speedManager);
-
-    float ultrasonicDistance = getUltrasonicDistance(&dp->ultrasonicSensor);
-
-    // Update the sensor feedback
-    char sensorFeedback[100];
-    sprintf(sensorFeedback, "Door: %s, IR Sensor: %s, Light: %s, Main: %s, Speed: %.2f cm/s, Ultrasonic: %.2f cm", doorStatus, irSensorStatus, lightStatus, mainStatus, currentSpeed, ultrasonicDistance);
-    updateSensorStatus(&dp->sensorFeedback, sensorFeedback);
+    // Set up server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 }
 
-// DoorControl.h
-#ifndef DOOR_CONTROL_H
-#define DOOR_CONTROL_H
-
-typedef struct {
-    char doorStatus[10];
-} DoorControl;
-
-void initDoorControl(DoorControl* dc);
-void closeDoor(DoorControl* dc);
-const char* getDoorStatus(DoorControl* dc);
-
-#endif  // DOOR_CONTROL_H
-
-// DoorControl.c
-#include "DoorControl.h"
-
-void initDoorControl(DoorControl* dc) {
-    strcpy(dc->doorStatus, "open");
+// Send packet function
+void sendPacket(const char *message) {
+    int sent_bytes = sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (sent_bytes == -1) {
+        perror("sendto failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Message sent: %s\n", message);
 }
 
-void closeDoor(DoorControl* dc) {
-    strcpy(dc->doorStatus, "closed");
+// Receive packet function
+void receivePacket(char *buffer) {
+    int recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &client_len);
+    if (recv_len == -1) {
+        perror("recvfrom failed");
+        exit(EXIT_FAILURE);
+    }
+    buffer[recv_len] = '\0';  // Null-terminate the received data
+    printf("Received message: %s\n", buffer);
 }
 
-const char* getDoorStatus(DoorControl* dc) {
-    return dc->doorStatus;
+// Handle station ID setting
+void setStationID(DataProcessing *dp, const char *stationID) {
+    strncpy(dp->stationID, stationID, MAX_KEY_LENGTH);
 }
 
-// IRSensor.h
-#ifndef IR_SENSOR_H
-#define IR_SENSOR_H
-
-typedef struct {
-    char irSensorStatus[10];
-} IRSensor;
-
-void initIRSensor(IRSensor* ir);
-void updateIRSensorStatus(IRSensor* ir, const char* status);
-const char* getIRSensorStatus(IRSensor* ir);
-
-#endif  // IR_SENSOR_H
-
-// IRSensor.c
-#include "IRSensor.h"
-
-void initIRSensor(IRSensor* ir) {
-    strcpy(ir->irSensorStatus, "unknown");
+// Retrieve station ID
+const char *getStationID(DataProcessing *dp) {
+    return dp->stationID;
 }
 
-void updateIRSensorStatus(IRSensor* ir, const char* status) {
-    strcpy(ir->irSensorStatus, status);
+// Function to execute commands based on received input
+void executeCommand(DataProcessing* dp, const char* command) {
+    if (strcmp(command, "FFASTC") == 0) {
+        setSpeed(&dp->speedManager, 5.0);
+        closeDoor(&dp->doorManager);
+    } else if (strcmp(command, "FSLOWC") == 0) {
+        setSpeed(&dp->speedManager, 2.5);
+        closeDoor(&dp->doorManager);
+    } else if (strcmp(command, "RSLOWC") == 0) {
+        setSpeed(&dp->speedManager, -2.5);
+        closeDoor(&dp->doorManager);
+    } else if (strcmp(command, "STOPC") == 0) {
+        setSpeed(&dp->speedManager, 0);
+        closeDoor(&dp->doorManager);
+    } else if (strcmp(command, "STOPO") == 0) {
+        setSpeed(&dp->speedManager, 0);
+        openDoor(&dp->doorManager);
+    } else if (strcmp(command, "DISCONNECT") == 0) {
+        sendCarriageMessage("OFLN");
+    } else {
+        printf("Unknown command: %s\n", command);
+    }
 }
 
-const char* getIRSensorStatus(IRSensor* ir) {
-    return ir->irSensorStatus;
-}
-
-// LightStatus.h
-#ifndef LIGHT_STATUS_H
-#define LIGHT_STATUS_H
-
-typedef struct {
-    char lightStatus[10];
-} LightStatus;
-
-void initLightStatus(LightStatus* ls);
-void updateLightStatus(LightStatus* ls, const char* status);
-const char* getLightStatus(LightStatus* ls);
-
-#endif  // LIGHT_STATUS_H
-
-// LightStatus.c
-#include "LightStatus.h"
-
-void initLightStatus(LightStatus* ls) {
-    strcpy(ls->lightStatus, "unknown");
-}
-
-void updateLightStatus(LightStatus* ls, const char* status) {
-    strcpy(ls->lightStatus, status);
-}
-
-const char* getLightStatus(LightStatus* ls) {
-    return ls->lightStatus;
-}
-
-// Main.h
-#ifndef MAIN_H
-#define MAIN_H
-
-typedef struct {
-    char mainStatus[10];
-} Main;
-
-void initMain(Main* m);
-void updateMainStatus(Main* m, const char* status);
-const char* getMainStatus(Main* m);
-
-#endif  // MAIN_H
-
-// Main.c
-#include "Main.h"
-
-void initMain(Main* m) {
-    strcpy(m->mainStatus, "unknown");
-}
-
-void updateMainStatus(Main* m, const char*){
-    
+// Function to send messages to the carriage control system
+void sendCarriageMessage(const char *message) {
+    printf("Sending message to Carriage Control Program: %s\n", message);
+    sendPacket(message);
 }
